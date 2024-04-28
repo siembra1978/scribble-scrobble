30f2fa9d1edb8e71179a4620fbf2023e71389f13
siembra <77788776+CheddarSand@users.noreply.github.com>
2024-04-28 12:23:24 -0400

    more progress stuff + batch run
    - more accurate ETA
    - renames window title to progress
    - (on windows) can be easily run by batch now

--- csvmanipulator.py
+++ csvmanipulator.py
@@ -4,6 +4,8 @@ import pandas as pd
 import searchmusic
 import searchtunes
 import os
+import subprocess
+import platform
 import time
 from progress.bar import Bar
 import datetime
@@ -11,9 +13,15 @@ import datetime
 importedCSV = "Apple Music Play Activity.csv"
 keepColumns =['Artists', 'Song Name', 'Album Name','Play Duration Milliseconds', 'Album Artist']
 scriptFile = os.path.dirname(os.path.abspath(__file__))
-maxFMRows = 57317
 rateLimit = 2
 
+def setTitle(title):
+    title = str(title)
+    if platform.system() == "Windows":
+        os.system(f"title {title}")
+    else:
+        sys.stdout.write(f"\x1b]2;{title}\x07")
+        sys.stdout.flush()
 def filterCSV(inputCSV):
     df = pd.read_csv(inputCSV)
     df = df[df['Media Duration In Milliseconds'] > 0]
@@ -24,15 +32,32 @@ def filterCSV(inputCSV):
     newAlbums = []
     artistsValues = []
 
+    lastIteration = [None]
+    timeTable = []
+    timeElapsed = 0
+    avgTimeDiff = 0
+
     df = df.reset_index(drop=True)
     rowNum = len(df)
 
-    bar = Bar('Finding Artists:', max=rowNum)
-    os.system('clear')
+    bar = Bar('| Finding Artists', max=rowNum)
+    setTitle('Finding Artists...')
+
+    subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
 
     errors = 0
 
     for index, row in df.iterrows():
+
+        now = time.time()
+
+        if lastIteration[0] is not None:
+            timeElapsed = now - lastIteration[0]
+            timeTable.append(timeElapsed)
+            avgTimeDiff = sum(timeTable) / len(timeTable)
+
+        lastIteration[0] = now
+
         artistValue = str(searchtunes.findArtist(row['Song Name'], row['Album Name']))
         if artistValue == 'No results found' or artistValue == 'Artist not found':
             errors += 1
@@ -41,8 +66,16 @@ def filterCSV(inputCSV):
         albumCorrection = str(row['Album Name'])
         newAlbums.append(albumCorrection)
 
-        bar.suffix = str(round(((index + 1) / rowNum * 100))) + '%% | ' + str(index+1) + '/' + str(rowNum) + ' | ETA: ' + str(datetime.timedelta(seconds = round((rowNum-(index+1))*rateLimit))) + ' | Errors: ' + str(errors)
+        # progress stuff
+        percentComplete = str(round(((index + 1) / rowNum * 100))) + '%'
+        indexedProgress = str(index+1) + '/' + str(rowNum)
+        estimatedTime = str(datetime.timedelta(seconds = round((rowNum-(index+1))*avgTimeDiff)))
+
+        bar.suffix = percentComplete + '% | ' + indexedProgress + ' | ETA: ' + estimatedTime + ' | Errors: ' + str(errors) + ' | '
+        setTitle(percentComplete + ' [] ETA: ' + estimatedTime)
+
         bar.next()
+
         time.sleep(rateLimit)
 
     bar.finish()
@@ -61,10 +94,12 @@ def saveCSV(i, df):
 def spliceAndFinalize(df):
     df = df[df['Artists'] != 'Artist not found']
     df = df[df['Artists'] != 'No results found']
-    print('Done!')
     chunks = [df[i:i + 3000] for i in range(0, df.shape[0], 3000)]
 
     for index, chunk in enumerate(chunks):
         saveCSV(index, chunk)
 
 filterCSV(importedCSV)
+print('Done!')
+setTitle("Done!")
+input('Press enter to exit: ')
